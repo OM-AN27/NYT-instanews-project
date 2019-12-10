2db33e120af345cb731e82a23dbaf1bd96784257
Omar Lomeli <o.a.1027@outlook.com>
2019-12-10 11:09:28 -0800

    finish responsive layout and start ajax

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-
+$(function() {
+      const key = '?api-key=zYRIOfPzk9AGjtTQovI1AIrPU5QJ0d7l';
+      const menuValue = $('select').val();
+      const url = 'https://api.nytimes.com/svc/topstories/v2/' + menuValue + '.json' + key;
+      
+})
 
 
 
