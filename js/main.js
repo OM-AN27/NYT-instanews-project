d86d66be556c25d4a3c9f8f060d6844adc5ad588
Omar Lomeli <o.a.1027@outlook.com>
2019-12-10 15:02:38 -0800

    successful new york times API request test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(function() {
       const key = '?api-key=zYRIOfPzk9AGjtTQovI1AIrPU5QJ0d7l';
       const menuValue = $('select').val();
+      const content = $('main');
       const url = 'https://api.nytimes.com/svc/topstories/v2/' + menuValue + '.json' + key;
-      
+
+
+      $.ajax ({
+          method: 'GET',
+          url: url
+
+      }).done(function(data){
+          $.each(data.results, function(key, value){
+             alert('seems to be working');
+
+          }).fail(function(){
+              alert('failed to load');
+              content.append('<strong><p class="fail-message"> FAILED TO LOAD, TRY AGAIN LATER</p></strong>')
+          })
+      })
 })
 
 
