35fefc23ed0743cdd7f55c87b5c25925db4693b4
Omar Lomeli <o.a.1027@outlook.com>
2019-12-11 13:16:49 -0800

    add variables folder and succesfully display news on website

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
-// $(function() {
-//       const key = '?api-key=zYRIOfPzk9AGjtTQovI1AIrPU5QJ0d7l';
-//       const menuValue = $('select').val();
-//       const content = $('main');
-//       const url = 'https://api.nytimes.com/svc/topstories/v2/' + menuValue + '.json' + key;
+$(function() {
+      const key = '?api-key=zYRIOfPzk9AGjtTQovI1AIrPU5QJ0d7l';
+      const menuValue = $('select').val();
+      const content = $('main');
+      const loading = $('.loader');
+      const nytUrl = 'https://api.nytimes.com/svc/topstories/v2/' + menuValue + '.json' + key;
+
+    //   menuValue.change(function(event){
+    //       event.preventDefault();
+
+    //   })
 
 
-//       $.ajax ({
-//           method: 'GET',
-//           url: url
+      $.ajax ({
+          method: 'GET',
+          url: nytUrl
 
-//       }).done(function(data){
-//           $.each(data.results, function(key, value){
+      }).always(function(){
+          loading.hide();
+      })
+      .done(function(data){
+          $.each(data.results, function(key, value){
+              content.append(`<section class='main-news' style='background-image: url(${value.multimedia[4].url});'><a href='${value.url}><div class='caption'>${data.results[key].abstract}</div></a></section>`)
+          })
+      }).fail(function(){
+        alert('Failed to load, try again later');
 
-//           }).fail(function(){
-//               alert('failed to load');
-//               content.append('<strong><p class="fail-message"> FAILED TO LOAD, TRY AGAIN LATER</p></strong>')
-//           })
-//       })
-// })
+      })
+});
+
+
 
 
 
