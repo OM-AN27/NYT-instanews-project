5375db0c157cb2940c9a9b2a0c19d22354ae587e
Omar Lomeli <o.a.1027@outlook.com>
2019-12-14 16:03:59 -0800

    fix issue on not showing more news on input change, add new photo for news that dont have one, clean not used code

--- js/main.js
+++ js/main.js
@@ -1,33 +1,45 @@
 $(function() {
-      const key = '?api-key=zYRIOfPzk9AGjtTQovI1AIrPU5QJ0d7l';
-      const menuValue = $('select').val();
-      const content = $('main');
-      const loading = $('.loader');
-      const nytUrl = 'https://api.nytimes.com/svc/topstories/v2/' + menuValue + '.json' + key;
 
-    //   menuValue.change(function(event){
-    //       event.preventDefault();
+    
+    const key = '?api-key=zYRIOfPzk9AGjtTQovI1AIrPU5QJ0d7l';
+    const mainContent = $('main');
+    const loading = $('.loader');
+    const header = $('header');
+    
+    $('select').on('change', function(){
+        mainContent.html('');
 
-    //   })
-
-
-      $.ajax ({
+        if ($('select').val() === 'none') {
+        return
+        } else {
+        
+        let nytUrl = 'https://api.nytimes.com/svc/topstories/v2/sports.json'  + key;
+        event.preventDefault();
+        nytUrl = nytUrl.replace('sports', $('select').val());
+        
+        
+        
+        $.ajax ({
           method: 'GET',
           url: nytUrl
+          
+        }).always(function(){
+            loading.hide();
+        })
+        .done(function(data){
+            $.each(data.results, function(key, value){
+                header.addClass('header-animation');
+                mainContent.append(`<section class='main-news' style='background-image: url(${value.multimedia[4]?value.multimedia[4].url: 'img/noPhotoLogo.jpg'});'><a href='${value.url}><div class='caption' style='background: rgba(0,0,0,0.7);'>${data.results[key].abstract}</div></a></section>`)
+            })
+        }).fail(function(){
+            alert('Failed to load, try again later');
+            
+        })
+    }
 
-      }).always(function(){
-          loading.hide();
-      })
-      .done(function(data){
-          $.each(data.results, function(key, value){
-              content.append(`<section class='main-news' style='background-image: url(${value.multimedia[4].url});'><a href='${value.url}><div class='caption'>${data.results[key].abstract}</div></a></section>`)
-          })
-      }).fail(function(){
-        alert('Failed to load, try again later');
-
-      })
 });
 
+});
 
 
 
